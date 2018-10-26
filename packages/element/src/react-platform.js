6eca307cf0938e657266344e07a9d13ed01a9983
jankimoradiya <janki.moradiya@gmail.com>
2018-10-26 13:02:07 +0530

    Need to add period(.) in documentation.
    Hello,
    
    Need to add period(.) in end of every sentence of the documentation of the file.
    
    Thanks

--- packages/element/src/react-platform.js
+++ packages/element/src/react-platform.js
@@ -11,32 +11,32 @@ import {
 /**
  * Creates a portal into which a component can be rendered.
  *
- * @see https://github.com/facebook/react/issues/10309#issuecomment-318433235
+ * @see https://github.com/facebook/react/issues/10309#issuecomment-318433235.
  *
- * @param {Component} component Component
- * @param {Element}   target    DOM node into which element should be rendered
+ * @param {Component} component Component.
+ * @param {Element}   target    DOM node into which element should be rendered.
  */
 export { createPortal };
 
 /**
- * Finds the dom node of a React component
+ * Finds the dom node of a React component.
  *
- * @param {Component} component component's instance
- * @param {Element}   target    DOM node into which element should be rendered
+ * @param {Component} component component's instance.
+ * @param {Element}   target    DOM node into which element should be rendered.
  */
 export { findDOMNode };
 
 /**
  * Renders a given element into the target DOM node.
  *
- * @param {WPElement} element Element to render
- * @param {Element}   target  DOM node into which element should be rendered
+ * @param {WPElement} element Element to render.
+ * @param {Element}   target  DOM node into which element should be rendered.
  */
 export { render };
 
 /**
  * Removes any mounted element from the target DOM node.
  *
- * @param {Element} target DOM node in which element is to be removed
+ * @param {Element} target DOM node in which element is to be removed.
  */
 export { unmountComponentAtNode };
